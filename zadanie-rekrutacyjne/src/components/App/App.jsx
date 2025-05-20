4d040a1f2f05bb6c1bff470910f99ac6ab8d1215
Konrad <kondykox@gmail.com>
2025-05-20 14:07:43 +0200

    hero section

---
+++ zadanie-rekrutacyjne/src/components/App/App.jsx
@@ -0,0 +1,30 @@
+import Navbar from "../Navbar/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <header className="hero section">
+        <div className="hero__panel">
+          <h1 className="hero__header">A Simple Bookmark Manager</h1>
+          <p className="hero__description">
+            A clean and simple interface to organize your favourite websites.
+            Open a new browser tab and see your sites load instantly. Try it for
+            free.
+          </p>
+          <div className="hero__btn__container">
+            <button className="btn btn-primary">Get it on Chrome</button>
+            <button className="btn btn-secondary">Get it on Firefox</button>
+          </div>
+        </div>
+        <div className="hero__image">
+          <img src="./images/illustration-hero.svg" alt="Hero image" />
+          <div className="rounded-bg" />
+        </div>
+      </header>
+    </main>
+  );
+}
+
+export default App;
